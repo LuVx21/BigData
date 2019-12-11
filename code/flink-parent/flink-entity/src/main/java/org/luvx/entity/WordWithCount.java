2af4a611a2634518c5d6fd8f89394ae5561101e4
RenXie <yeyu0926@hotmail.com>
2019-12-11 20:13:21 +0800

    update

--- code/flink-parent/flink-entity/src/main/java/org/luvx/entity/WordWithCount.java
+++ code/flink-parent/flink-entity/src/main/java/org/luvx/entity/WordWithCount.java
@@ -7,16 +7,17 @@ import lombok.NoArgsConstructor;
  * @ClassName: org.luvx.common.entity
  * @Description:
  * @Author: Ren, Xie
- * @Date: 2019/12/10 18:22
  */
 @AllArgsConstructor
 @NoArgsConstructor
-        public class WordWithCount {
+public class WordWithCount {
     public String word;
     public long   cnt;
 
     @Override
     public String toString() {
-        return "WC " + word + " " + cnt;
+        return "WordWithCount:" +
+                "word='" + word + '\'' +
+                ", cnt=" + cnt;
     }
 }
