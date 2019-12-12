28c50efad64a541c42846c65d4566e4cf0474065
RenXie <yeyu0926@hotmail.com>
2019-12-12 22:20:52 +0800

    update

--- code/flink-parent/flink-table/src/main/java/org/luvx/batch/BatchMain.java
+++ code/flink-parent/flink-table/src/main/java/org/luvx/batch/BatchMain.java
@@ -1,4 +1,4 @@
-package org.luvx.wc;
+package org.luvx.batch;
 
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
@@ -11,17 +11,33 @@ import org.luvx.entity.WordWithCount;
  * @Description:
  * @Author: Ren, Xie
  */
-public class WordCountTableMain {
+public class BatchMain {
 
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         BatchTableEnvironment tEnv = BatchTableEnvironment.create(env);
 
         DataSet<WordWithCount> input = env.fromElements(
-                new WordWithCount("Hello", 1),
-                new WordWithCount("Ciao", 1),
-                new WordWithCount("Hello", 1));
+                new WordWithCount("foo", 1),
+                new WordWithCount("bar", 1),
+                new WordWithCount("foo", 1));
 
+        sql(tEnv, input);
+        // table(tEnv, input);
+    }
+
+    public static void sql(BatchTableEnvironment tEnv, DataSet<WordWithCount> input) throws Exception {
+        tEnv.registerDataSet("WordCount", input, "word, cnt");
+
+        Table table = tEnv.sqlQuery(
+                "select word, sum(cnt) as cnt from WordCount group by word having sum(cnt) = 2");
+
+        DataSet<WordWithCount> result = tEnv.toDataSet(table, WordWithCount.class);
+
+        result.print();
+    }
+
+    public static void table(BatchTableEnvironment tEnv, DataSet<WordWithCount> input) throws Exception {
         Table table = tEnv.fromDataSet(input);
 
         Table filtered = table
@@ -30,7 +46,6 @@ public class WordCountTableMain {
                 .filter("cnt = 2");
 
         DataSet<WordWithCount> result = tEnv.toDataSet(filtered, WordWithCount.class);
-
         result.print();
     }
 }
