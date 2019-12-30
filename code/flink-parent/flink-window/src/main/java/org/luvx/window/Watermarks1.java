dbff343122c340ff78b9e1e1fee8824f75c6cbf7
F.LuVx <yeyu0926@hotmail.com>
2019-12-30 16:38:09 +0800

    flink  window

--- code/flink-parent/flink-window/src/main/java/org/luvx/window/Watermarks1.java
+++ code/flink-parent/flink-window/src/main/java/org/luvx/window/Watermarks1.java
@@ -1,6 +1,5 @@
 package org.luvx.window;
 
-
 import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.api.watermark.Watermark;
 import org.luvx.entity.LogEvent;
@@ -14,6 +13,10 @@ import javax.annotation.Nullable;
  */
 public class Watermarks1 implements AssignerWithPeriodicWatermarks<LogEvent> {
     private       Long currentMaxTimestamp = 0L;
+    /**
+     * 最大允许乱序时间
+     * 窗口结束时间后再加上这个时间开始关闭窗口进行计算
+     */
     private final Long maxOutOfOrderness   = 10 * 1000L;
 
     @Nullable
--- code/flink-parent/flink-window/src/main/java/org/luvx/window/WindowMain.java
+++ code/flink-parent/flink-window/src/main/java/org/luvx/window/WindowMain.java
@@ -3,6 +3,7 @@ package org.luvx.window;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -11,8 +12,9 @@ import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
-import org.luvx.entity.LogEvent;
+import org.apache.flink.util.OutputTag;
 import org.luvx.common.utils.DateTimeUtils;
+import org.luvx.entity.LogEvent;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -20,13 +22,28 @@ import java.util.List;
 
 /**
  * @ClassName: org.luvx
- * @Description: kc -lk 9000  使用request_log.txt中数据
+ * @Description: nc -lk 9000  使用 request_log.txt 中数据
+ * 窗口等待3秒, 窗口大小为10s, 每5s计算一次
+ * 水位线 会随着 事件时间的增长而增长
+ * <pre>
+ *     测试数据事件以每秒形式进入, 起点为2015-05-17 10:05:01:000
+ *
+ * 结果:
+ *    | 触发窗口时间 |           窗口范围          |       事件范围      | 个数 |
+ *    | 10:05:08:000 | 10:04:55:000 ~ 10:05:05:000 | 10:05:01 ~ 10:05:04 |  4   |
+ *    | 10:05:13:000 | 10:05:00:000 ~ 10:05:10:000 | 10:05:01 ~ 10:05:09 |  9   |
+ *    | 10:05:18:000 | 10:05:05:000 ~ 10:05:15:000 | 10:05:05 ~ 10:05:14 | 10   |
+ *
+ * 窗口内事件有可能超过窗口大小, 如一个窗口关闭后, 有一新事件在这个窗口内, 会触发窗口重新计算, 此时事件数量为size+1
+ *
+ * 时间恰好在window end上的事件不纳入窗口的计算
+ * </pre>
  * @Author: Ren, Xie
  */
 @Slf4j
 public class WindowMain {
-    private static final String host = "192.168.224.129";
-    private static final int    port = 9000;
+    private static final String host = "211.159.175.179";
+    private static final int    port = 59000;
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -48,7 +65,9 @@ public class WindowMain {
                 new BoundedOutOfOrdernessTimestampExtractor<LogEvent>(Time.seconds(3)) {
                     @Override
                     public long extractTimestamp(LogEvent element) {
-                        log.info("当前水位线:{}", DateTimeUtils.epochMilli2Time(getCurrentWatermark().getTimestamp()));
+                        log.info("当前事件时间: {}, 水位线: {}", DateTimeUtils.epochMilli2Time(element.getEventTime()),
+                                DateTimeUtils.epochMilli2Time(getCurrentWatermark().getTimestamp())
+                        );
                         return element.getEventTime();
                     }
                 }
@@ -56,8 +75,14 @@ public class WindowMain {
 
         operator.print("luvx");
 
+        // 接收延迟事件
+        OutputTag<LogEvent> tag = new OutputTag<LogEvent>("late") {
+        };
+
         SingleOutputStreamOperator<Long> operator1 = operator
                 .timeWindowAll(Time.seconds(10), Time.seconds(5))
+                .allowedLateness(Time.seconds(10))
+                .sideOutputLateData(tag)
                 .apply(
                         new AllWindowFunction<LogEvent, Long, TimeWindow>() {
                             @Override
@@ -69,7 +94,7 @@ public class WindowMain {
                                     LogEvent log = it.next();
                                     logs.add(log);
                                 }
-                                log.info("所有事件({}个事件):{}", logs.size(), logs);
+                                log.info("{}个事件:{}", logs.size(), logs);
                                 log.info("窗口时间:{} ~ {}", DateTimeUtils.epochMilli2Time(window.getStart()), DateTimeUtils.epochMilli2Time(window.getEnd()));
                                 log.info("----------------- 窗口汇总↑ -----------------");
 
@@ -77,6 +102,9 @@ public class WindowMain {
                             }
                         }
                 );
+        DataStream<LogEvent> late = operator1.getSideOutput(tag);
+        late.print("late");
+
         operator1.print("count");
 
         env.execute("window test example");
