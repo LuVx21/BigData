4ebce0ef5411a5058a9cc9a61be37251d8f8857f
F.LuVx <yeyu0926@hotmail.com>
2020-01-09 00:06:27 +0800

    init

---
+++ code/flink-parent/flink-table/src/main/resources/sql/query3.sql
@@ -0,0 +1,7 @@
+INSERT INTO pvuv_sink
+SELECT
+  DATE_FORMAT(TO_TIMESTAMP(CAST(ts as VARCHAR)), 'yyyy-MM-dd HH:00') dt,
+  COUNT(*) AS pv,
+  COUNT(DISTINCT user_id) AS uv
+FROM user_log
+GROUP BY DATE_FORMAT(TO_TIMESTAMP(CAST(ts as VARCHAR)), 'yyyy-MM-dd HH:00');
